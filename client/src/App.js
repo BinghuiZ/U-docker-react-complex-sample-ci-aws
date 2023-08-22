57a44d41904568f1bcfda8d4468ee000b9a27f65
Benjamin, Zhong Binghui <zhongbinghui1995@gmail.com>
2023-08-22 15:35:23 -0400

    chore: second try

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React; Deploy to AWS
+            Learn React - Second try
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
